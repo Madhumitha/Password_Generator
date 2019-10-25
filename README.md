881103c0996e9a9ccbdbc48faa390425ebbf8440
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2019-10-25 14:31:26 -0500

    Password Generator

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Password_Generator
+
+### Description 
+
+An application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code.
+
+The user will be prompted to choose from the following password criteria:
+
+1. Length (must be between 8 and 128 characters)
+
+2. Character type:
+
+    1. [Special characters] (https://www.owasp.org/index.php/Password_special_characters)
+    2. Numeric characters
+    3. Lowercase characters
+    4. Uppercase characters
+
+The application validates user input and ensures that at least one character type is selected.
+
+Once all prompts are answered, the user will be presented with a password matching the answered prompts. Displaying the generated password to the page.
+
+The user also have the option to click a button to copy the password to their clipboard.
+
+In addition to that, the application adapts to multiple screen sizes.
+
+![Screenshot]()
+
+
+[Random Password Generator]()
+
+
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+// Author: Madhumitha Prabakaran 
+
+
+
 var pass_length = prompt("Length of password? (must be between 8 and 128 characters)");
 
-var char_type = prompt("Character type : Type 'S': Special character/Type 'L' : Lowercase/Type 'U' : Uppercase/Type 'N' : Number ")
+var char_type = prompt("Enter Character type without space: Type 'S': Special character/Type 'L' : Lowercase/Type 'U' : Uppercase/Type 'N' : Number ")
 
 // Validate the character type
 
@@ -14,75 +18,102 @@ if(char_type == null)
 var char_array = char_type.split("");
 console.log(char_array);
 
-// Generating random lowercase, uppercase, special character or number 
+// Typecast 
 
-function getRandomLowercase()
+const num = parseInt(pass_length);
+console.log(num);
+
+
+let flag_L = 0; 
+let flag_U = 0;
+let flag_N = 0;
+let flag_S =  0;
+
+// Character type functions 
+
+if(char_array[0] == 'U' || char_array[1] == 'U' || char_array[2] == 'U' || char_array[3] == 'U')
 {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-console.log(getRandomLowercase());
-
-function getRandomUppercase()
+     flag_U = 1;
+    function getRandomUppercase()
 {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 console.log(getRandomUppercase());
-
-function getRandomNumber()
-{
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-console.log(getRandomNumber());
 
-function getRandomSymbol() 
-{
-    const symbols = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~' ;
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-console.log(getRandomSymbol());
+ if (char_array[0] == 'L' || char_array[1] == 'L' || char_array[2] == 'L' || char_array[3] == 'L')
+{   
+     flag_L = 1;
 
-
-for(var i=0; i<char_array.length; i++)
-{
-    if(char_array[i] === 'S')
+    function getRandomLowercase()
     {
-        var special = getRandomSymbol();
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
     }
-    if(char_array[i] === 'U')
-    {
-        var upper = getRandomUppercase();
-    }
-    if(char_array[i] === 'L')
-    {
-        var lower = getRandomLowercase();
-    }   
-    if(char_array[i] === 'N')
-    {
-        var number = getRandomNumber();
-    }    
+    
+    console.log(getRandomLowercase());
 }
-
-
-// Typecast 
-
-var num = parseInt(pass_length);
-console.log(num);
+ if (char_array[0] == 'N' || char_array[1] == 'N' || char_array[2] == 'N' || char_array[3] == 'N')
+{   
+     flag_N = 1;
+    
+    function getRandomNumber()
+    {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    }
+    console.log(getRandomNumber());
+}
+if (char_array[0] == 'S' || char_array[1] == 'S' || char_array[2] == 'S' || char_array[3] == 'S')
+{   
+     flag_S = 1;
+    
+    function getRandomSymbol() 
+    {
+        const symbols = '!@#$%^&*(){}[]+<>?<>' ;
+        return symbols[Math.floor(Math.random() * symbols.length)];
+    }
+    console.log(getRandomSymbol());
+}
 
 
 // Password Generator 
 
 function generate() {
-    let values = special + upper + lower + number ;
+    let password = '';
+    
 
-    let password = " ";
-
-    
-
-    for(var i=0; i <= num; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+   for(let i=0; i < num; i++) {
+        if (flag_S == 1)
+        {
+           password += getRandomSymbol();
+        }
+        if (flag_L == 1)
+        {
+           password += getRandomLowercase();
+        }
+        if (flag_U == 1)
+        {
+           password += getRandomUppercase();
+        }
+        if (flag_N == 1)
+        {
+           password += getRandomNumber();
+        }
     }
 
-    document.getElementById("display").value = password;
+    console.log(password);
+
+    document.getElementById("display").value = password;
 
 }
+
+// Copy password to clipboard 
+
+function copyToClipboard(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied!");
+}
+
